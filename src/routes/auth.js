dbc37d60586207bbd957a2ec34a2089dfe618b49
macfluffy <shadowimitation@hotmail.com>
2025-11-26 20:26:04 +1100

    feat: defined the create a task route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,6 @@
 // Import Packages
 const express= require("express");
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const Task = require("../models/Task");
 
 const router = express.Router();
 
 const auth = require('../middlewares/auth');
-
 router.use(auth);
 
-router.post("/", (request, response) => {
+router.post("/", async (request, response) => {
     try {
         const {title, status, dueDate} = response.body;
 
@@ -17,6 +17,14 @@ router.post("/", (request, response) => {
             dueDate,
             user: request.user.userId   // Link the task to the current user
         });
+
+        // Save the task
+        console.log(task);
+        await task.save();
+        reponse.status(201).json({
+            message: "New task added",
+            body: task
+        })
     } catch (error) {
         response.status(400).json({
             error: error.message
